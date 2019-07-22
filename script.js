dd603c8c5ed83359168cf735e848882245051eb4
ojaoc <c0rreia@hotmail.com>
2019-07-22 01:50:31 +0100

    flipping img

--- script.js
+++ script.js
@@ -10,6 +10,30 @@ const rock_div = document.getElementById('rock');
 const paper_div = document.getElementById('paper');
 const scissors_div = document.getElementById('scissors');
 
+const rock_img = document.getElementById('r');
+const paper_img = document.getElementById('p');
+const scissors_img = document.getElementById('s');
+
+
+rock_div.onmouseover = () => {
+    rock_img.style.visibility = 'hidden';
+};
+paper_div.onmouseover = () => {
+    paper_img.style.visibility = 'hidden';
+};
+scissors_div.onmouseover = () => {
+    scissors_img.style.visibility = 'hidden';
+};
+rock_div.onmouseout = () => {
+    rock_img.style.visibility = 'visible';
+};
+paper_div.onmouseout = () => {
+    paper_img.style.visibility = 'visible';
+};
+scissors_div.onmouseout = () => {
+    scissors_img.style.visibility = 'visible';
+};
+
 function main() {
     rock_div.addEventListener('click', function() {
     game('rock');
