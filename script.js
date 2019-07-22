c54d379d1581deaeee17cf4ef9f0821a10f08c46
ojaoc <c0rreia@hotmail.com>
2019-07-22 02:17:08 +0100

    flipping hand done

--- script.js
+++ script.js
@@ -14,24 +14,59 @@ const rock_img = document.getElementById('r');
 const paper_img = document.getElementById('p');
 const scissors_img = document.getElementById('s');
 
+let rockBack_img = document.createElement('img');
+rockBack_img.src = "./img/rock-back.png";
+let paperBack_img = document.createElement('img');
+paperBack_img.src = "./img/paper-back.png";
+let scissorsBack_img = document.createElement('img');
+scissorsBack_img.src = "./img/scissors-back.png";
+
+rockBack_img.style.visibility = 'hidden';
+paperBack_img.style.visibility = 'hidden';
+scissorsBack_img.style.visibility = 'hidden';
+
+rockBack_img.style.zindex = -1;
+paperBack_img.style.zindex = -1;
+scissorsBack_img.style.zindex = -1;
+rockBack_img.style.position = 'absolute';
+paperBack_img.style.position = 'absolute';
+scissorsBack_img.style.position = 'absolute';
+rockBack_img.style.right = 0;
+paperBack_img.style.right = 0;
+scissorsBack_img.style.right = 0;
+rockBack_img.style.top = 0;
+paperBack_img.style.top = 0;
+scissorsBack_img.style.top = 0;
+
+
+rock_div.appendChild(rockBack_img);
+paper_div.appendChild(paperBack_img);
+scissors_div.appendChild(scissorsBack_img);
+
 
 rock_div.onmouseover = () => {
     rock_img.style.visibility = 'hidden';
+    rockBack_img.style.visibility = 'visible';
 };
 paper_div.onmouseover = () => {
     paper_img.style.visibility = 'hidden';
+    paperBack_img.style.visibility = 'visible';
 };
 scissors_div.onmouseover = () => {
     scissors_img.style.visibility = 'hidden';
+    scissorsBack_img.style.visibility = 'visible';
 };
 rock_div.onmouseout = () => {
     rock_img.style.visibility = 'visible';
+    rockBack_img.style.visibility = 'hidden';
 };
 paper_div.onmouseout = () => {
     paper_img.style.visibility = 'visible';
+    paperBack_img.style.visibility = 'hidden';
 };
 scissors_div.onmouseout = () => {
     scissors_img.style.visibility = 'visible';
+    scissorsBack_img.style.visibility = 'hidden';
 };
 
 function main() {
